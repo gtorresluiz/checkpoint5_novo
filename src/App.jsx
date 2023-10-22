9c293e8ff6ce97c0122d1535783b83e3472bfeb0
gtorresluiz <rm98600@fiap.com.br>
2023-10-22 19:55:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import './style.scss';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [produtos, setProdutos] = useState([]);
+  const [nomeProduto, setNomeProduto] = useState('');
+  const [precoProduto, setPrecoProduto] = useState('');
+  const [produtoId, setProdutoId] = useState('');
+
+  useEffect(() => {
+    // Carregar produtos da API ao carregar a página
+    fetch('http://localhost:5000/produtos')
+      .then((response) => response.json())
+      .then((data) => setProdutos(data))
+      .catch((error) => console.error('Erro ao carregar produtos da API:', error));
+  }, []);
+
+  const inserirProduto = () => {
+    // Enviar um novo produto para a API com o método POST
+    fetch('http://localhost:5000/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nome: nomeProduto, preço: parseFloat(precoProduto) }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Atualizar a lista de produtos após a inserção
+        setProdutos([...produtos, data]);
+        setNomeProduto('');
+        setPrecoProduto('');
+      })
+      .catch((error) => console.error('Erro ao inserir o produto:', error));
+  };
+
+  const excluirProduto = () => {
+    // Enviar uma solicitação DELETE para a API com base no ID do produto
+    fetch(`http://localhost:5000/produtos/${produtoId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Remover o produto da lista após a exclusão
+          setProdutos(produtos.filter((p) => p.id !== parseInt(produtoId)));
+          setProdutoId('');
+        } else {
+          console.error('Erro ao excluir o produto.');
+        }
+      })
+      .catch((error) => console.error('Erro ao realizar a requisição DELETE:', error));
+  };
+
+  return (
+    <div>
+      <h1>Gerenciamento de Produtos</h1>
+      <div>
+        <h2>Inserir Produto</h2>
+        <input
+          type="text"
+          placeholder="Nome do Produto"
+          value={nomeProduto}
+          onChange={(e) => setNomeProduto(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Preço do Produto"
+          value={precoProduto}
+          onChange={(e) => setPrecoProduto(e.target.value)}
+        />
+        <button onClick={inserirProduto}>Inserir</button>
+      </div>
+      <div>
+        <h2>Excluir Produto</h2>
+        <input
+          type="text"
+          placeholder="ID do Produto"
+          value={produtoId}
+          onChange={(e) => setProdutoId(e.target.value)}
+        />
+        <button onClick={excluirProduto}>Excluir</button>
+      </div>
+      <div>
+        <h2>Lista de Produtos</h2>
+        <ul>
+          {produtos.map((p) => (
+            <li key={p.id}>
+              {p.nome} - R${p.preço}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
